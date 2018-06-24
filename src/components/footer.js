2a24ad491fe871530ad40737e89232f149c8b9d4
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-06-24 16:56:20 +0600

    Link changed

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return (
     <footer className="footer-area">
-      <p>Made using <span><a href="https://vuejs.org/">React JS</a></span> by <span><a href="https://zonayed.me">Zonayed Ahmed</a></span></p>
+      <p>Made using <span><a href="https://reactjs.org/">React JS</a></span> by <span><a href="https://zonayed.me">Zonayed Ahmed</a></span></p>
     </footer>
   )
 }
